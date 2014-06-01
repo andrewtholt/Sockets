e72ddfbe6fadc3cba5876c5807a65f5ba7ff8cc0
andrewtholt <andrewtholt60@gmail.com>
2014-06-01 18:38:03 +0100

    Added missing header files.

--- helper.c
+++ helper.c
@@ -17,6 +17,10 @@ Programming", W Richard Stevens (Prentice Hall).
 #include <sys/socket.h>
 #include <unistd.h>
 #include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+
 
 
 /* Read a line from a socket  */
